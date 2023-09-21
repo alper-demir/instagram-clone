e488af2c06a71d83280722391671a1d60f7a235e
Alper <alper.demir.fener@gmail.com>
2023-09-21 23:00:32 +0300

    ana sayfa responsive olacak şekilde düzenlendi

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,10 +4,10 @@ import Siderbar from "../components/Siderbar"
 const Home = () => {
     return (
         <div className="flex h-screen">
-            <div className="w-[17.65%]">
+            <div className="w-[17.65%] max-xl:w-[73px] max-md:w-0">
                 <Siderbar />
             </div>
-            <div className="w-[82.35%]">
+            <div className="w-[82.35%] max-[1160px]:w-full max-xl:w-full">
                 <Outlet />
             </div>
         </div>
