305f835ce9b4ab0d9af6049df434e847fe31c88a
Alper <alper.demir.fener@gmail.com>
2025-02-23 02:58:37 +0300

    Update MainLayout.jsx
    Belirli sayfalarda Footer.jsx'i koşullu render ediyoruz.

--- client/src/layout/MainLayout.jsx
+++ client/src/layout/MainLayout.jsx
@@ -1,9 +1,11 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import Siderbar from "../components/Siderbar";
 import Footer from './../components/Footer';
 import { useEffect } from "react";
 
 const MainLayout = () => {
+    const location = useLocation();
+    const isChatPage = location.pathname.startsWith("/direct");
 
     useEffect(() => {
         const storedTheme = localStorage.getItem("theme");
@@ -13,20 +15,18 @@ const MainLayout = () => {
             document.documentElement.classList.remove("dark");
         }
     }, []);
-    
+
     return (
         <div className="flex h-screen bg-light-bg dark:bg-dark-bg text-light-text-primary dark:text-dark-text-primary">
             <div className="w-[17.65%] max-lg:w-[74px] border-r-[1px] border-light-border dark:border-dark-border">
                 <Siderbar />
             </div>
             <div className="w-[82.35%] max-lg:w-[calc(100%-74px)] overflow-x-hidden">
-                <div className="flex flex-col justify-between md:gap-y-32">
-                    <div className="w-full mx-auto">
+                <div className={`flex flex-col ${isChatPage ? "h-full" : "justify-between md:gap-y-32"}`}>
+                    <div className="w-full mx-auto flex-1">
                         <Outlet />
                     </div>
-                    <div>
-                        <Footer />
-                    </div>
+                    {!isChatPage && <Footer />}
                 </div>
             </div>
         </div>
