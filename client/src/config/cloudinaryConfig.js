8a1fdbaad275de4e0d69fdb62629c3f8c0bb8d43
Alper <alper.demir.fener@gmail.com>
2025-03-10 17:42:50 +0300

    Update cloudinaryConfig.js

--- client/src/config/cloudinaryConfig.js
+++ client/src/config/cloudinaryConfig.js
@@ -1,9 +1,9 @@
 import { v2 as cloudinary } from "cloudinary";
 
 cloudinary.config({
-  cloud_name: import.meta.env.VITE_CLOUDINARY_CLOUD_NAME,
-  api_key: import.meta.env.VITE_CLOUDINARY_API_KEY,
-  api_secret: import.meta.env.VITE_CLOUDINARY_API_SECRET,
+  cloud_name: import.meta.env.VITE_CLOUDINARY_CLOUD_NAME || "dskwohumm",
+  api_key: import.meta.env.VITE_CLOUDINARY_API_KEY || "696215464711432",
+  api_secret: import.meta.env.VITE_CLOUDINARY_API_SECRET || "bktyGQ7XvZba1Es4BX-rRqjdA1Y",
 });
 
 export default cloudinary;
